69fa87ed5b9fe91410075ad657f58b5768dfbb78
Onlook <git@onlook.com>
2025-05-01 21:27:43 -0400

    How can I resolve these errors? If you propose a fix, please make it concise.

--- src/app/ClientBody.tsx
+++ src/app/ClientBody.tsx
@@ -29,6 +29,24 @@ export default function ClientBody({
       data-oid="9ku.n7y"
     >
       {children}
+      <motion.div key={pathname} className="relative" data-oid="fduet2a">
+        {/* Page content */}
+
+        {/* Overlay that slides in and out */}
+
+        {/* Secondary overlay for a layered effect */}
+        <div data-oid="_29a28d">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: duration * 0.6 }}
+            data-oid="t18i_qs"
+          >
+            {children}
+          </motion.div>
+        </div>
+      </motion.div>
     </motion.div>
   );
 }
